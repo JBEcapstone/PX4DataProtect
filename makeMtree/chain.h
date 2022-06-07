f41ce80e614398b530cb9fdbc2c3ef424966d516
unknown <sju0924@khu.ac.kr>
2022-06-07 13:40:36 +0900

    Add chain, hashing block

--- makeMtree/chain.h
+++ makeMtree/chain.h
@@ -1,5 +1,85 @@
 #pragma once
+
+#include<stdint.h>
+#include"block.h"
 class chain
 {
+	struct chain_node {
+		block b;
+		chain_node* prev;
+	};
+public:
+
+	chain() {
+		leaf = (chain_node *)malloc(sizeof(chain_node));
+		leaf->b = block();
+		genesis = leaf;
+	}
+	/*
+		체인을 file로부터 import 한다.
+		@input: filename
+		@output: -
+	*/
+	void chain_import() {
+
+	}
+
+	/*
+		체인을 file로 export 한다.
+		@input: -
+		@output: -
+	*/
+	void chain_export() {
+
+	}
+
+	/*
+		체인에 데이터를 추가한다.
+		@input: 추가할 데이터 data
+	*/
+	void add(uint8_t data[], uint32_t timestamp) {
+		uint8_t blockhash[SHA256_DIGEST_VALUELEN];
+		if (!leaf->b.add(data, timestamp)) {
+			chain_node *new_chain = (chain_node *)malloc(sizeof(chain_node));
+			new_chain->b = block();
+			leaf->b.makehash(blockhash);
+
+			new_chain->b.setPrevHash(blockhash);
+			new_chain->prev = leaf;
+
+			leaf = new_chain;
+			leaf->b.add(data, timestamp);
+		}		
+	}
+
+	int verify(uint32_t timestamp, uint8_t data[]) {
+		chain_node* trace = NULL;
+		uint8_t prevHash[SHA256_DIGEST_VALUELEN], curHash[SHA256_DIGEST_VALUELEN];
+		cur = leaf;
+		
+		while (cur->prev != NULL && cur->prev->b.getTimeStamp() > timestamp) {
+			trace = cur;
+			cur = cur->prev;
+		}
+		
+		//genesis block이 아닐 때 
+		if (cur->prev != NULL) {
+
+			//다음 블럭에서 해시값 가져오기
+			trace->b.getPrevHash(prevHash);
+
+			//현재 블럭 해시하기
+			cur->b.makehash(curHash);
+			if (strcmp((char*)prevHash, (char*)curHash)) {
+				printf("블럭의 해시값이 다릅니다.\n");
+				return -1;
+			}
+		}		
+
+		return cur->b.verify(timestamp, data);
+	}
+
+private:
+	chain_node* genesis , *leaf, *cur ;
 };
 
--- makeMtree/makeMtree.cpp
+++ makeMtree/makeMtree.cpp
@@ -1,11 +1,11 @@
 ﻿#include <stdio.h>
-#include "hmap.h"
 #include "block.h"
+#include "chain.h"
 
 uint8_t *data[257];
 int main()
 {
-    block b;
+   chain c;
     for (int i = 0; i <= 256; i++) {
         data[i] = (uint8_t*)malloc(sizeof(uint8_t) * 10);
         for (int j = 0; j < 10; j++) {
@@ -13,27 +13,20 @@ int main()
             else data[i][j] = 97 + (i * j) % 26;
         }
 
-        b.add(data[i], i + 1);
+        c.add(data[i], i + 1);
     }
 
 
-    uint8_t *data2[6];
-    for (int i = 0; i < 6; i++) {
-        data2[i] = (uint8_t *)malloc(sizeof(uint8_t)*10);
-        for (int j = 0; j < 10; j++) {
-            data2[i][j] = data[i + 24][j];
-        }
-        
-    }
-    
+    /*
     uint8_t nisemono[10] = { 'a','b','c','d','e','f','g','h','f','\0' };
 
-    printf("%d~%d번째 노드 검증결과: %d\n", 25,30, b.verify(25,30, data2));
+    printf("%d~%d번째 노드 검증결과: %d\n", 25,30, .verify(25,30, data2));
     printf("%d~%d번째 노드 검증결과: %d\n", 26, 31, b.verify(26, 31, data2));
-    printf("%d~%d번째 노드 검증결과: %d\n", 1, 256, b.verify(1, 256, data));
+    printf("%d~%d번째 노드 검증결과: %d\n", 1, 256, b.verify(1, 256, data));*/
     //printf("%d번째 노드 검증결과: %d\n", 25, b.verify(25, nisemono));
    
   /* for (int i = 0; i <= 256; i++) {
        printf("%d번째 노드 검증결과: %d\n", i+1, b.verify(i+1, data[i]));
    }*/
+
 }
